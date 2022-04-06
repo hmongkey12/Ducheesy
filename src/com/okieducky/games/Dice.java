3a3b45a4354cedd2b93d2dcfd8cfee3d711aaaef
Khammadao Lee <khammadaolee@Khammadaos-MacBook-Pro.local>
2022-04-06 13:34:24 -0700

    changed dice count

--- src/com/okieducky/games/Dice.java
+++ src/com/okieducky/games/Dice.java
@@ -1,7 +1,7 @@
 package com.okieducky.games;
 
 public class Dice {
-    private static final int DICE_MAX = 6;
+    private static final int DICE_MAX = 4;
 
     public static int rollDice() {
         return (int) ((Math.random() * DICE_MAX) + 1);
